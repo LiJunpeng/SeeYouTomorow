9d577fe98da3f4d5bbccd7974e407745acb91ddd
LILouis <lijunpeng91@gmail.com>
2017-04-18 20:51:20 -0400

    timepicker works

--- SeeYouTomorrow/app/src/main/java/com/example/louis/seeyoutomorrow/MainActivity.java
+++ SeeYouTomorrow/app/src/main/java/com/example/louis/seeyoutomorrow/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.louis.seeyoutomorrow;
 import android.app.AlarmManager;
 import android.app.DialogFragment;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,10 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     RadioButton rdiNotification, rdiToast;
     Button btnOneTime, btnRepeating;
@@ -33,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 DialogFragment dialogFragment = new TimePickerFragment();
                 dialogFragment.show(getFragmentManager(), "timePicker");
+
 //                if (rdiNotification.isChecked()) {
 //                    startAlarm(true, false);
 //                } else {
@@ -53,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 //        });
     }
 
-    private void startAlarm(boolean isNotification, boolean isRepeat, Calendar calendar) {
+    private void startAlarm(boolean isNotification, boolean isRepeat, int hour, int minute) {
         AlarmManager manager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
         Intent myIntent;
         PendingIntent pendingIntent;
@@ -79,6 +83,11 @@ public class MainActivity extends AppCompatActivity {
 //        calendar.set(Calendar.SECOND, 30);
 //        calendar.set(Calendar.MILLISECOND, 0);
 
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+
         myIntent = new Intent(MainActivity.this, AlarmNotificationReceiver.class);
         pendingIntent = PendingIntent.getBroadcast(this, 0, myIntent, 0);
 
@@ -87,6 +96,20 @@ public class MainActivity extends AppCompatActivity {
         manager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                 pendingIntent);
 
+    }
+
+
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Toast.makeText(this, "当前时间设置为： " + hourOfDay + " : " + minute, Toast.LENGTH_LONG).show();
+        //Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_LONG).show();
+//
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+//        calendar.set(Calendar.MINUTE, minute);
+
+        startAlarm(true, true, hourOfDay, minute);
     }
 
 }
--- SeeYouTomorrow/app/src/main/java/com/example/louis/seeyoutomorrow/TimePickerFragment.java
+++ SeeYouTomorrow/app/src/main/java/com/example/louis/seeyoutomorrow/TimePickerFragment.java
@@ -1,11 +1,10 @@
 package com.example.louis.seeyoutomorrow;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.widget.TimePicker;
-import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -13,8 +12,25 @@ import java.util.Calendar;
  * Created by Louis on 4/17/17.
  */
 
-public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+public class TimePickerFragment extends DialogFragment {
     private Calendar calendar;
+
+    private Activity mActivity;
+    private TimePickerDialog.OnTimeSetListener mListener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mActivity = activity;
+
+        // This error will remind you to implement an OnTimeSetListener
+        //   in your Activity if you forget
+        try {
+            mListener = (TimePickerDialog.OnTimeSetListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnTimeSetListener");
+        }
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -22,12 +38,16 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
         calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int minute = calendar.get(Calendar.MINUTE);
-        return new TimePickerDialog(getActivity(), this, hour, minute, true);
+        return new TimePickerDialog(mActivity, mListener, hour, minute, true);
+//        return new TimePickerDialog(mActivity, mListener, hour, minute,
+//                DateFormat.is24HourFormat(mActivity));
     }
-    @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        Toast.makeText(getActivity(), "当前时间设置为： " + hourOfDay + " : " + minute, Toast.LENGTH_LONG).show();
-    }
+//    @Override
+//    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//        //Toast.makeText(getActivity(), "当前时间设置为： " + hourOfDay + " : " + minute, Toast.LENGTH_LONG).show();
+//        //Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_LONG).show();
+//
+//
+//    }
 
-    public get
 }
